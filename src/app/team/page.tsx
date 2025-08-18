c9e883699a2f9d5de7692933ea9ebbd227f82b61
The_Br34Ch_Head <shwet.op08@gmail.com>
2025-08-18 10:16:04 +0000

    Not the button bruh! Make the button blue and the text over button white

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -74,7 +74,7 @@ export default function TeamPage() {
       <BackgroundDecorations />
       <main className="relative z-10 container mx-auto px-4 py-20 lg:py-24 text-center">
         <div className="absolute top-8 left-8">
-            <Button asChild variant="ghost" className="text-white hover:text-neon-blue">
+            <Button asChild className="bg-neon-blue text-white hover:bg-neon-blue/80">
                 <Link href="/"><ArrowLeft className="mr-2 h-4 w-4" /> Return to HQ</Link>
             </Button>
         </div>
