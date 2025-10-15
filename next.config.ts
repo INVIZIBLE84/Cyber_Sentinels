23393f54a1b3c9093887b5f3b7c28e08951f1335
Shwet Shirbhayye <shwet.op08@gmail.com>
2025-10-15 16:19:16 +0000

    See, I set the Github-Actions in the source but it's showing the README

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Cyber_Sentinels',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
